6fb6fdf28168ea3a83b6ef871c514d079645aa85
Christopher Aziz <caziz@ucla.edu>
2018-10-15 22:07:17 -0700

    Add Toggle Component

---
+++ js/components/Toggle.js
@@ -0,0 +1,39 @@
+// @flow
+
+import * as React from 'react'
+import styled from 'styled-components'
+import { colors } from 'styles'
+
+const ToggleFrame = styled.div`
+  width: 44px;
+  height: 25px;
+  border: 3px solid ${colors.blue};
+  background: ${colors.blue};
+  border-radius: 6px;
+  column-count: 2;
+  column-gap: 0px;
+  opacity: ${props => (props.toggled ? 1 : 0.5)};
+`
+
+const ToggleInner = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  background-color: white;
+  opacity: ${props => (props.toggled ? 1 : 0)};
+  transition: opacity 0.3s ease;
+`
+
+type PropsType = {
+  toggled: boolean,
+  onClick: () => void
+}
+
+export default function Toggle(props: PropsType): React.Element<*> {
+  return (
+    <ToggleFrame toggled={props.toggled} onClick={props.onClick}>
+      <ToggleInner toggled={!props.toggled} />
+      <ToggleInner toggled={props.toggled} />
+    </ToggleFrame>
+  )
+}
