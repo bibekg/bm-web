d5e8172e2fb758515db428d64e0ca9252b89d3ef
Alex Zhao <alexzhao21@gmail.com>
2018-10-29 20:11:17 -0700

    combine two instrs into one and add giant explanation comment

--- js/components/FoldSwitch.jsx
+++ js/components/FoldSwitch.jsx
@@ -31,10 +31,23 @@ const FoldedText = Text.extend`
   margin: 0;
   text-align: center;
 
+  /**
+   * To ensure fadein/out that doesn't leave the element interactable (e.g. highlightable,
+   * picked up by screen readers, etc), we set visibility to visible/hidden depending
+   * on tbe state of isFolded.
+   * However, this ruins the opacity animation, so we need to "transition" visibility
+   * before opacity so that the element becomes visible before becoming opaque and
+   * it becomes transparent before becoming hidden. Thus we set a transition-delay
+   * equal to the length of the opacity animation so this is possible.
+   * HOWEVER, this transition delay will mess up when fading in, delaying for 0.3 seconds
+   * before suddenly appearing in. To solve this, on fade in we need to reset the
+   * transition delay to 0 so that the element instantly becomes visible before
+   * actually performing the opacity animation.
+   */
+
   visibility: ${props => (props.isFolded ? 'visible' : 'hidden')};
   opacity: ${props => (props.isFolded ? 1 : 0)}; /* 1 if folded, 0 if not */
-  transition: visibility 0s linear 0.3s, opacity 0.3s linear;
-  ${props => props.isFolded && 'transition-delay: 0s'};
+  transition: visibility 0s linear ${props => (props.isFolded ? 0 : 0.3)}s, opacity 0.3s ease;
 `
 
 function FoldSwitch(props: PropsType): React.Element<*> {
