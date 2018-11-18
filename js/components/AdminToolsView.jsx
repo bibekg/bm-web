32a3b5a3537ee52e668d43090178858b3d44a680
Bibek <ghim.bibek@gmail.com>
2018-11-18 13:33:07 -0800

    Rearchitect tools page to be one-tool-at-a-time

--- js/components/AdminToolsView.jsx
+++ js/components/AdminToolsView.jsx
@@ -2,11 +2,13 @@
 
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Route, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import PageContainer from 'components/PageContainer'
 import { Header, Subtitle, Title, Text } from 'components/typography'
+import { CreateMatch, EditMatch, DeleteUser, makeToolPage } from 'components/AdminTools'
 import Break from 'components/Break'
-import { EditMatch, DeleteUser, CreateNewMatch, DeleteMatch } from 'components/AdminTools'
+import { colors } from 'styles'
 import * as actions from 'actions'
 
 const ViewWrapper = PageContainer({ noBackground: true, maxWidth: { large: '800px' } }).extend`
@@ -26,6 +28,34 @@ const Content = styled.div`
   align-items: flex-start;
 `
 
+const LinkRow = styled.div`
+  display: flex;
+  > * {
+    margin-right: 10px;
+  }
+`
+
+const StyledNavLink = styled(NavLink)`
+  padding: 10px;
+  text-decoration: none;
+  border-radius: 3px;
+  border: 2px solid ${colors.blue};
+  ${Text} {
+    color: black;
+  }
+
+  &.active {
+    background-color: ${colors.blue};
+    ${Text} {
+      color: ${colors.white};
+    }
+  }
+`
+
+const ToolWrapper = styled.div`
+  width: 100%;
+`
+
 type PropsType = {
   getAllMatches: () => void,
   getAllUsers: () => void
@@ -37,6 +67,13 @@ class AdminToolsView extends React.Component<PropsType> {
 
     props.getAllUsers()
     props.getAllMatches()
+  }
+
+  makeToolPageRenderer(Tool, { title, subtitle }) {
+    return () => {
+      const ToolPage = makeToolPage(Tool)
+      return <ToolPage title={title} subtitle={subtitle} />
+    }
   }
 
   render(): React.Element<*> {
@@ -52,24 +89,51 @@ class AdminToolsView extends React.Component<PropsType> {
           <Subtitle align="left">-- Steve Jobs</Subtitle>
         </Top>
 
+        <Break />
+
         <Content>
-          <Break />
+          <Text bold align="left">
+            Select a tool to get started...
+          </Text>
 
-          <Title align="left">Match Custom Users</Title>
-          <Text paragraph>{'Match two users with each other.'}</Text>
-          <CreateNewMatch />
+          <LinkRow>
+            <StyledNavLink to="/admin/tools/create-match">
+              <Text bold>Create Match</Text>
+            </StyledNavLink>
+            <StyledNavLink to="/admin/tools/edit-match">
+              <Text bold>Edit Match</Text>
+            </StyledNavLink>
+            <StyledNavLink to="/admin/tools/delete-user">
+              <Text bold>Delete User</Text>
+            </StyledNavLink>
+          </LinkRow>
 
-          <Break />
-
-          <Title align="left">Edit a match</Title>
-          <Text paragraph>{'Edit a match between two users, or delete the match altogether.'}</Text>
-          <EditMatch />
-
-          <Break />
-
-          <Title align="left">Hard Delete a User</Title>
-          <Text paragraph>{"Completely delete a user, along with all of the user's matches from the system."}</Text>
-          <DeleteUser />
+          <ToolWrapper>
+            <Route
+              exact
+              path="/admin/tools/create-match"
+              render={this.makeToolPageRenderer(CreateMatch, {
+                title: 'Create Match',
+                subtitle: 'Match two users with each other.'
+              })}
+            />
+            <Route
+              exact
+              path="/admin/tools/edit-match"
+              render={this.makeToolPageRenderer(EditMatch, {
+                title: 'Edit Match',
+                subtitle: 'Edit a match between two users, or delete the match altogether.'
+              })}
+            />
+            <Route
+              exact
+              path="/admin/tools/delete-user"
+              render={this.makeToolPageRenderer(DeleteUser, {
+                title: 'Delete Match',
+                subtitle: "Completely delete a user, along with all of the user's matches from the system."
+              })}
+            />
+          </ToolWrapper>
         </Content>
       </ViewWrapper>
     )
