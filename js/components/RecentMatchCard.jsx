69f77609a2a9b779a8945059a68569b04c6b474d
Jack Van Boening <jackvanboening@gmail.com>
2019-03-14 20:16:41 -0700

    Init - Recent Matches Page w/ Array of Match Cards (#53)
    * Init - Recent Matches Page w/ Array of Match Cards
    
    * Add link to Recent Matches in NavBar

---
+++ js/components/RecentMatchCard.jsx
@@ -0,0 +1,150 @@
+// @flow
+
+import * as React from 'react'
+import styled from 'styled-components'
+import AnimateHeight from 'react-animate-height'
+import Card from 'components/Card'
+import { Text, Title } from 'components/typography'
+import UserImage from 'components/UserImage'
+import ProfileTextInfo from 'components/ProfileTextInfo'
+import { colors, breakpoints } from 'styles'
+import { formatRelationshipType } from 'utilities/user-formatters'
+import { copy } from 'product-copy'
+import FoldSwitch from 'components/FoldSwitch'
+
+const ProfileCardDiv = Card.extend`
+  padding: 30px;
+  padding-bottom: ${props => (props.foldable ? 0 : 30)}px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`
+
+const InfoBannerDiv = styled.div`
+  display: flex;
+  align-items: center;
+  & > * {
+    padding: 20px;
+  }
+
+  @media (max-width: ${breakpoints.profileCard - 1}px) {
+    flex-flow: column nowrap;
+  }
+  @media (min-width: ${breakpoints.profileCard}px) {
+    flex-flow: row nowrap;
+  }
+`
+
+const DetailedInfoColumns = styled.div`
+  display: flex;
+
+  @media (max-width: ${breakpoints.profileCard - 1}px) {
+    flex-flow: column nowrap;
+    align-items: flex-start;
+    padding: 0px;
+  }
+  @media (min-width: ${breakpoints.profileCard}px) {
+    flex-flow: row nowrap;
+    padding: 20px;
+
+    & > * {
+      flex: 1;
+      &:not(:last-child) {
+        border-right: 1px solid ${colors.lightGrey};
+        padding-right: 20px;
+      }
+
+      &:not(:first-child) {
+        padding-left: 20px;
+      }
+    }
+  }
+`
+
+const BoldSpanText = styled.span`
+  font-weight: 400;
+  text-align: left;
+`
+
+const DetailText = Text.extend`
+  margin-top: 5px;
+  text-align: left;
+`
+
+// Foldedness is described in a prop (foldable) and a state (isFolded). isFolded will
+// start as true or false depending on if the prop is also set to true or false.
+// This way, for things like AutoDate, we set foldable to true and the card will start
+// as folded, and for the rest, foldable is false and isFolded isn't relevant.
+
+type PropsType = {
+  user: UserType,
+  matchBasis: ?Array<UserRelationshipType>,
+  foldable: boolean
+}
+
+type StateType = {
+  isFolded: boolean
+}
+
+class RecentMatchCard extends React.Component<PropsType, StateType> {
+  static defaultProps = {
+    foldable: false
+  }
+
+  constructor(props: PropsType) {
+    super(props)
+    this.state = {
+      isFolded: props.foldable // so we start folded if foldable and not if not
+    }
+  }
+
+  handleFold = () => {
+    this.setState({
+      isFolded: !this.state.isFolded
+    })
+  }
+
+  render(): ?React.Element<*> {
+    const { bio } = this.props.user
+
+    return (
+      <ProfileCardDiv foldable={this.props.foldable}>
+        <InfoBannerDiv>
+          <div>
+            <UserImage src={this.props.user.profilePic} />
+          </div>
+          <ProfileTextInfo user={this.props.user} hideContactInfo={false} />
+        </InfoBannerDiv>
+
+        <AnimateHeight duration={300} height={this.state.isFolded ? 0 : 'auto'}>
+          <DetailedInfoColumns>
+            <div>
+              <Title align="left">{copy.matchCard.about}</Title>
+              <BoldSpanText>{copy.matchCard.bio}</BoldSpanText>
+              {bio &&
+                bio.split('\n').map(p => (
+                  <DetailText paragraph key={p}>
+                    {p}
+                  </DetailText>
+                ))}
+              <br />
+              <BoldSpanText>{copy.matchCard.looking}</BoldSpanText>
+              {/* this implementation assumes there is always an intersection in relationship types */}
+              <DetailText paragraph>
+                {this.props.matchBasis && this.props.matchBasis.length > 0
+                  ? this.props.matchBasis.map(formatRelationshipType).join(', ')
+                  : 'any'}
+              </DetailText>
+            </div>
+          </DetailedInfoColumns>
+        </AnimateHeight>
+
+        {this.props.foldable && (
+          <FoldSwitch isFolded={this.state.isFolded} onClick={this.handleFold} text={copy.foldSwitch.text} />
+        )}
+      </ProfileCardDiv>
+    )
+  }
+}
+
+export default RecentMatchCard
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -5,10 +5,12 @@ import authenticationReducer from './authentication'
 import userReducer from './user'
 import matchReducer from './match'
 import adminReducer from './admin'
+import recentReducer from './recent'
 
 export default combineReducers({
   auth: authenticationReducer,
   user: userReducer,
+  recent: recentReducer,
   admin: adminReducer,
   match: matchReducer
 })
